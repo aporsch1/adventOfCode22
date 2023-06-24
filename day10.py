8b39102154e70b8b3b2f30d227f9fb197ad43092
Abe Porschet <abeporschet@gmail.com>
2023-06-24 13:14:54 -0400

    unfinished day10 stuff

---
+++ day10.py
@@ -0,0 +1,16 @@
+import numpy as np
+file  = open("day10.txt", "r")
+lines =  file.readlines()
+
+def main():
+    X = 1
+    cycle = 0
+    for i in lines:
+        if i=='noop':
+            cycle=cycle+1
+        else:
+           i. split(' ')
+           cycle=cycle+2
+           X=X+i[1]
+
+
