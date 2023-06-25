2962fd6282fd0af31590ca3c1f226ee5325d2930
Abe Porschet <abeporschet@gmail.com>
2023-06-25 16:50:49 -0400

    still frustrated with this but I need to leave and work on my laptop

--- day10.py
+++ day10.py
@@ -1,16 +1,28 @@
-import numpy as np
 file  = open("day10.txt", "r")
-lines =  file.readlines()
+lines =  file.read().strip().splitlines()
 
 def main():
     X = 1
-    cycle = 0
+    cycle = 1
+    accum = 0
     for i in lines:
-        if i=='noop':
-            cycle=cycle+1
-        else:
-           i. split(' ')
-           cycle=cycle+2
-           X=X+i[1]
+        cycle+=1
+        if cycle % 40 == 20:
+            accum = accum + X
+        if i.startswith('addx '):
+            if cycle % 40 == 20:
+                accum = accum + X
+            cycle = cycle + 1
+            if cycle % 40 == 20:
+                accum = accum + X
+           #print(int(i[5:]))
+            X=X+int(i[5:])
+        if cycle % 40 == 20:
+            accum = accum + 1
+        print(accum)
+    print(cycle)
+    print(accum)
+    
+main()
 
 
