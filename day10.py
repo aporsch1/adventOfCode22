7eee5421b9a4aed2018c9dadb571e58208ac53f0
Abe Porschet <abeporschet@gmail.com>
2023-06-25 20:57:16 -0400

    finally finished day 10, took some help from reddit

--- day10.py
+++ day10.py
@@ -1,28 +1,36 @@
-file  = open("day10.txt", "r")
-lines =  file.read().strip().splitlines()
-
-def main():
-    X = 1
-    cycle = 1
-    accum = 0
-    for i in lines:
-        cycle+=1
-        if cycle % 40 == 20:
-            accum = accum + X
-        if i.startswith('addx '):
-            if cycle % 40 == 20:
-                accum = accum + X
-            cycle = cycle + 1
-            if cycle % 40 == 20:
-                accum = accum + X
-           #print(int(i[5:]))
-            X=X+int(i[5:])
-        if cycle % 40 == 20:
-            accum = accum + 1
-        print(accum)
-    print(cycle)
-    print(accum)
-    
-main()
-
-
+actions = [lines.split(" ")
+           for lines in open("day10.txt", "r").read().splitlines()]
+cycle = 1
+index = 0
+register = 1
+currentcyle = []
+numbers = 0
+final = 0
+while cycle < 241:
+    if actions[index][0] == 'noop':
+        if len(currentcyle) == 1:
+            index += 1
+            currentcyle.clear()
+            continue
+        else:
+            currentcyle.append(cycle)
+    if actions[index][0] == 'addx':
+        if len(currentcyle) == 2:
+            register += int(actions[index][1])
+            index += 1
+            currentcyle.clear()
+            continue
+        else:
+            currentcyle.append(cycle)
+    if cycle % 40 == 20:
+        numbers += cycle*register
+    if (((cycle-1) % 40) == register+1 or ((cycle-1) % 40) == register or ((cycle-1) % 40) == register-1):
+        print('#', end='')
+    else:
+        print(' ', end='')
+    if (cycle % 40 == 0):
+        print('')
+    if (cycle == 220):
+        final = numbers
+    cycle += 1
+print(final)
