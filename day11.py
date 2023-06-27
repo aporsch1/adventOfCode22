380f1dcd6d43aec2bfb91eee8edbf7112036329f
Abe Porschet <abeporschet@gmail.com>
2023-06-26 21:36:56 -0400

    no clue what I've added or subtracted. Further than I thought. Just pushing to be careful

--- day11.py
+++ day11.py
@@ -1,6 +1,6 @@
 #this is going to be so weird and I am tired and feel like I can't read or comprehend anything right now so this will be an absolute riot.
-
-class monkey:
+from tqdm import tqdm
+class Monkey:
     def __init__(self, index: int, item_list: list[int], operation: str, division_test: int, send_list: list[int]):
         self.index = index
         self.items = item_list
@@ -10,7 +10,7 @@ class monkey:
         self.total_items_checked = 0
 
     def __repr__(self):
-        return f"Monkey(index={self.index}, items={self.items}, operation={self.operation}, division_test={self.division_test}, send_indices={self.send_indices})"
+        return f"Monkey(index={self.index}, items={self.items}, operation={self.operation}, division_test={self.test}, send_indices={self.send_indices})"
 
     def update_items(self):
         self.items = [eval(self.operation) // 3 for old in self.items]
@@ -31,7 +31,7 @@ def parse_monkeys(input_lines):
     indices, items, operations, division_tests, send_indices = [], [], [], [], []
     for line in lines:   
         if line.startswith('Monkey'):
-            indices.append(int(line.strip[-2]))
+            indices.append(int(line.strip()[-2]))
         elif line.strip().startswith('Starting items:'):
             items.append([int(el) for el in line.strip().replace("Starting items: ", "").split(",")])
         elif line.strip().startswith('Operation'):
@@ -39,7 +39,7 @@ def parse_monkeys(input_lines):
         elif line.strip().startswith('Test'):
             division_tests.append(int(line.strip().replace("Test: divisible by ", "")))
         elif line.strip().startswith('If true:'):
-            current_send_indices.append[int(line.strip().replace("If true: throw to monkey ", ""))]
+            current_send_indices = [int(line.strip().replace("If true: throw to monkey ", ""))]
             send_indices.append(current_send_indices)
         elif line.strip().startswith('If false:'):
             current_send_indices.append(int(line.strip().replace("If false: throw to monkey ", "")))
@@ -47,3 +47,22 @@ def parse_monkeys(input_lines):
     return [Monkey(*args) for args in zip(indices, items, operations, division_tests, send_indices)]
 
 monkeys = parse_monkeys(lines)
+
+def simulate_system(monkey_list: list[Monkey]):
+    for monkey in monkey_list:
+        monkey.update_items()
+        dict_to_send = monkey.monkey_send()
+        for key in dict_to_send.keys():
+            monkey_list[key].receive_items(dict_to_send[key])
+    return monkey_list
+
+def solution_one(monkey_list,nrounds):
+    for _ in range(nrounds):
+        monkey_list = simulate_system(monkeys)
+        print(monkey_list)
+    top2_monkeys = sorted([monkey.total_items_checked for monkey in monkey_list])[-2:]
+    print(sorted([monkey.total_items_checked for monkey in monkey_list]))
+    return top2_monkeys[0]*top2_monkeys[1]
+
+print(solution_one(monkeys, 20))
+
